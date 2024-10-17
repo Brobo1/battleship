546d2f81f649042f760347ec2b601770279797de
Brobo1 <armanahz95@gmail.com>
2024-10-18 00:12:08 +0200

    Initialize player board in constructor
    Add a board property to the Player class, initializing it with a new Board instance. This ensures each player has a board immediately upon creation.

--- src/player.js
+++ src/player.js
@@ -1,6 +1,9 @@
+import { Board } from "./board";
+
 class Player {
   constructor(name, playerType) {
     this.name = name;
     this.playerType = playerType;
+    this.board = new Board();
   }
 }

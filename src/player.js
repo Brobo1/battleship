cbf3291139ebe31ca002d33314f7d2368fd4754b
Brobo1 <armanahz95@gmail.com>
2024-10-18 21:02:38 +0200

    Add default playerType and AI functionalities
    Set playerType default to "computer" and add methods for AI ship placement and hitting. The placeShip and makeHit methods now handle random coordinates for AI players, enhancing computer player behavior.

--- src/player.js
+++ src/player.js
@@ -1,9 +1,31 @@
 import { Board } from "./board";
 
 class Player {
-  constructor(name, playerType) {
+  constructor(name, playerType = "computer") {
     this.name = name;
     this.playerType = playerType;
     this.board = new Board();
+  }
+
+  placeShip(x, y, ship, direction) {
+    if (this.playerType === "computer") {
+      this.board.placeShip(
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10),
+        ship,
+        direction,
+      );
+    } else {
+      this.board.placeShip(x, y, ship, direction);
+    }
+  }
+
+  makeHit(x, y) {
+    if (this.playerType === "computer") {
+      this.board.hit(
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10),
+      );
+    } else this.board.hit(x, y);
   }
 }

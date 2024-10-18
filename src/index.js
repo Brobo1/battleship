58fc73fae66c1e8b045e78f4fa24c9629bfbe0b4
Brobo1 <armanahz95@gmail.com>
2024-10-18 21:41:11 +0200

    Refactor HTML structure and initialize player boards
    Updated the HTML to include a grids container for better organization. In JavaScript, restructured player initialization and added functions to create and assign player boards dynamically.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import "./styles.css";
 import { Player } from "./player";
 
-const player1 = new Player("Human", "human");
-const player2 = new Player("Comp", "computer");
+const container = document.getElementById("container");
+const grids = document.getElementById("grids");
+
+const players = {
+  p1: new Player("Human", "human"),
+  p2: new Player("Computer", "computer"),
+};
+
+function createBoard(player) {
+  let grid = document.createElement("div");
+  grid.className = "grid";
+
+  for (const cells of player.board.board) {
+    for (const cell of cells) {
+    }
+  }
+
+  grids.appendChild(grid);
+}
+
+function assignBoard() {
+  for (const player in players) {
+    createBoard(players[player]);
+  }
+}
+// assignBoard();
+createBoard(players.p1);

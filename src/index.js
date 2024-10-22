fb540abb44e304f74686a901fdd0d0fd408113f1
Brobo1 <armanahz95@gmail.com>
2024-10-22 20:18:31 +0200

    Add ship rotation for placement
    Introduced a rotation button to toggle ship orientation between horizontal and vertical. Updated highlight and placement logic to accommodate the new rotation feature. Ensured proper feedback on mouse events.

--- src/index.js
+++ src/index.js
@@ -29,9 +29,14 @@ function assignBoard() {
 }
 
 function highlightCells(cell, color, x, y, rot = true) {
-  let coord = { x: cell.dataset.row, y: cell.dataset.col };
-  let shipLen = players.p1.avaiableShips.find((item) => item.placed === false)
-    .ship.length;
+  let shipToPlace = players.p1.avaiableShips.find(
+    (item) => item.placed === false,
+  );
+
+  if (!shipToPlace) return false;
+
+  let shipLen = shipToPlace.ship.length;
+
   x = parseInt(x);
   y = parseInt(y);
 
@@ -50,10 +55,22 @@ function highlightCells(cell, color, x, y, rot = true) {
   }
 }
 
+let isRotate = false;
+
+rotateBtn.addEventListener("click", () => {
+  isRotate = !isRotate;
+});
+
 gridsDiv.addEventListener("mouseover", (e) => {
   const cell = e.target;
   if (cell.className === "cell") {
-    highlightCells(cell, "#ffffff", cell.dataset.row, cell.dataset.col, false);
+    highlightCells(
+      cell,
+      "#ffffff",
+      cell.dataset.row,
+      cell.dataset.col,
+      isRotate,
+    );
   }
 });
 
@@ -62,7 +79,7 @@ gridsDiv.addEventListener("click", (e) => {
   if (cell.className === "cell") {
     const row = parseInt(cell.dataset.row, 10);
     const col = parseInt(cell.dataset.col, 10);
-    players.p1.placeShip(row, col, "h");
+    players.p1.placeShip(row, col, isRotate ? "h" : "v");
     assignBoard();
   }
 });
@@ -70,7 +87,13 @@ gridsDiv.addEventListener("click", (e) => {
 gridsDiv.addEventListener("mouseout", (e) => {
   const cell = e.target;
   if (cell.className === "cell") {
-    highlightCells(cell, "#353535", cell.dataset.row, cell.dataset.col, false);
+    highlightCells(
+      cell,
+      "#353535",
+      cell.dataset.row,
+      cell.dataset.col,
+      isRotate,
+    );
   }
 });
 

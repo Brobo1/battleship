36e5b06101689522ac12beda367a8cdecf34d8c5
Brobo1 <armanahz95@gmail.com>
2024-10-22 18:15:37 +0200

    Disable 'placeShip' and 'makeHit' methods in Player class
    Commented out the 'placeShip' and 'makeHit' methods in the Player class to temporarily prevent their execution. These methods are no longer invoked, likely for debugging or refactoring purposes.

--- src/classes/player.js
+++ src/classes/player.js
@@ -15,27 +15,27 @@ class Player {
     ];
   }
 
-  placeShip(x, y, ship, direction) {
-    if (this.playerType === "computer") {
-      this.board.placeShip(
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-        ship,
-        direction,
-      );
-    } else {
-      this.board.placeShip(x, y, ship, direction);
-    }
-  }
+  // placeShip(x, y, ship, direction) {
+  //   if (this.playerType === "computer") {
+  //     this.board.placeShip(
+  //       Math.floor(Math.random() * 10),
+  //       Math.floor(Math.random() * 10),
+  //       ship,
+  //       direction,
+  //     );
+  //   } else {
+  //     this.board.placeShip(x, y, ship, direction);
+  //   }
+  // }
 
-  makeHit(x, y) {
-    if (this.playerType === "computer") {
-      this.board.hit(
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-      );
-    } else this.board.hit(x, y);
-  }
+  // makeHit(x, y) {
+  //   if (this.playerType === "computer") {
+  //     this.board.hit(
+  //       Math.floor(Math.random() * 10),
+  //       Math.floor(Math.random() * 10),
+  //     );
+  //   } else this.board.hit(x, y);
+  // }
 }
 
 export { Player };

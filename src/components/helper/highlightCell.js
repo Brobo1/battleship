edcdead07be0f005d635e3bb00c98c90237c7677
Brobo1 <armanahz95@gmail.com>
2024-10-23 16:28:10 +0200

    Refactor board setup and ship placement logic
    Modularize the board setup and ship placement by moving related functions to their respective files. Introduce highlightCells to handle cell highlighting and create assignBoard to reset and assign boards. This improves code clarity and separates concerns for easier maintenance and future updates.

---
+++ src/components/helper/highlightCell.js
@@ -0,0 +1,24 @@
+export function highlightCells(cell, color, x, y, player, rot) {
+  let shipToPlace = player.avaiableShips.find((item) => item.placed === false);
+
+  if (!shipToPlace) return false;
+
+  let shipLen = shipToPlace.ship.length;
+
+  x = parseInt(x);
+  y = parseInt(y);
+
+  if (x + shipLen > 10 && rot) x = 10 - shipLen;
+  if (y + shipLen > 10 && !rot) y = 10 - shipLen;
+
+  for (let i = 0; i < shipLen; i++) {
+    let q;
+    if (rot) {
+      q = document.querySelector(`[data-row='${x + i}'][data-col='${y}']`);
+    } else {
+      q = document.querySelector(`[data-row='${x}'][data-col='${y + i}']`);
+    }
+    if (q && !q.classList.contains("cell-ship"))
+      q.style.backgroundColor = color;
+  }
+}
